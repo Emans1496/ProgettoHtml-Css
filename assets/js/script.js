c8b803e557868e52dca71495417d4490f9c6b21a
Emans1496 <emanuelesquillante@libero.it>
2024-03-31 22:37:51 +0200

    hotel pomelia

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,24 @@ function sendMail() {
 
   emailjs.send('service_6g5fnea', 'template_2ivri26', templateParams)
     .then(function() {
-
+      // Alert Bootstrap personalizzato per indicare che il messaggio è stato inviato con successo
       let alertDiv = document.createElement("div");
       alertDiv.classList.add("alert", "alert-success");
       alertDiv.setAttribute("role", "alert");
       alertDiv.textContent = "Your message has been sent!";
       
-     )
+      // Aggiungi l'alert al contenitore dell'alert (sostituisci "alertContainer" con l'ID del tuo contenitore)
       document.getElementById("alertContainer").appendChild(alertDiv);
 
+      // Rimuovi l'alert dopo un certo periodo di tempo (opzionale)
       setTimeout(function() {
         alertDiv.remove();
-      }, 5000);
+      }, 5000); // Rimuovi l'alert dopo 5 secondi (5000 millisecondi)
     })
     .catch(function(error) {
-
+      // Alert per indicare che si è verificato un errore durante l'invio del messaggio
       alert("Si è verificato un errore durante l'invio del messaggio: " + error);
     });
-}
-
-function toggleMenu() {
-  var menu = document.getElementById("navbarNav");
-  if (menu.classList.contains("menu-collapsed")) {
-      menu.classList.remove("menu-collapsed");
-      menu.classList.add("menu-expanded");
-  } else {
-      menu.classList.remove("menu-expanded");
-      menu.classList.add("menu-collapsed");
-  }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
